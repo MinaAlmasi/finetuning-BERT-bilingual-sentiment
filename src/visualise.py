f5f80b31e80ffcbb57d85fa4090423ff12a27cf4
MinaAlmasi <minaalmasi00@gmail.com>
2023-05-22 13:34:45 +0200

    fix comments

--- src/visualise.py
+++ src/visualise.py
@@ -15,8 +15,7 @@ import pandas as pd
 # custom functions
 from modules.visualisation_fns import (create_metrics_dataframes, create_table,
                                         load_model_histories, plot_model_histories,
-                                        plot_confusion_matrices
-                                 )
+                                        plot_confusion_matrices)
 
 def main(): 
     # define paths
@@ -33,7 +32,7 @@ def main():
     # plot model histories
     plot_model_histories(model_histories, visualisationspath / "loss_curves_all_models.png")
 
-    # metrics
+    # metrics, create dataframes for each model, include all metrics files ("all", "eng", "es")
     metrics_dfs = create_metrics_dataframes(resultspath / "metrics", metrics_to_include="metrics")
 
     # define headers for table
@@ -46,7 +45,7 @@ def main():
     with open(visualisationspath / "all_models_metrics_table.txt", "w") as file:
         file.write(table)
 
-    # plot confusion matrices
+    # plot confusion matrices for each model
     matrices = plot_confusion_matrices(resultspath / "predictions", visualisationspath, preds_to_include="all_predictions")
 
 if __name__ == "__main__":
